6c2d1b5504b39fa87352abeef839b503ff9b3b73
Mathias Almeida de Siqueira <mathias.adsiqueira@gmail.com>
2026-08-27 18:32:12 -0300

    Fix stock management methods in Produto class

--- Semana02/Exercicio_classe01.py
+++ Semana02/Exercicio_classe01.py
@@ -9,6 +9,7 @@ class Produto:
         print("Nome do produto:", self.nome)
         print("Preço do produto:", self.preco)
         print("Quantidade do produto:", self.qtd_estoque)
+        print("Valor total em estoque:R$", self.calcular_valor())
 
     def adicionar_produto(self):
          qtd_aumentar = int(input("Quantia de items adicionados: "))
@@ -16,7 +17,7 @@ class Produto:
 
     def vender_produto(self):
         venda = int(input("Quantia de items vendidos: "))
-        self.qtd_estoque - venda
+        self.qtd_estoque -= venda
 
         if venda > self.qtd_estoque:
             print("Venda não disponivel, devido a falta de estoque")
@@ -24,8 +25,38 @@ class Produto:
             print("Venda realizada com sucesso")
 
     def calcular_valor(self):
-        valor = self.qtd_estoque * self.preco
-        
+        return  self.qtd_estoque * self.preco
+         
 banana = Produto("Banana", 5, 30)
 maca = Produto("Maça", 6, 20)
 abacaxi = Produto("Abacaxi", 8, 10)
+
+
+banana = Produto("Banana", 5, 30)
+maca = Produto("Maçã", 6, 20)
+abacaxi = Produto("Abacaxi", 8, 10)
+
+
+print("=== VENDA DE BANANAS ===")
+banana.vender_produto()
+
+print("=== VENDA DE MAÇÃS ===")
+maca.vender_produto()
+
+print("=== VENDA DE ABACAXIS ===")
+abacaxi.vender_produto()
+
+print("=== REPOSIÇÃO DE BANANAS ===")
+banana.adicionar_produto()
+
+print("=== REPOSIÇÃO DE MAÇÃS ===")
+maca.adicionar_produto()
+
+print("=== REPOSIÇÃO DE ABACAXIS ===")
+abacaxi.adicionar_produto()
+
+print("===== ESTOQUE ATUALIZADO =====")
+
+banana.mostrar_produto()
+maca.mostrar_produto()
+abacaxi.mostrar_produto()
