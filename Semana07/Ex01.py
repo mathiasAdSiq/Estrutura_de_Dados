cfea8b819517492d85c96520836ae2ebc8095d11
Mathias Almeida de Siqueira <mathias.adsiqueira@gmail.com>
2026-09-17 19:57:44 -0300

    Implement linked list for player management

---
+++ Semana07/Ex01.py
@@ -0,0 +1,67 @@
+
+class No:
+
+    def __init__(self, nome):
+        self.nome = nome
+        self.proximo = None
+
+
+def adicionar_nome(lista, nome):
+    novo = No(nome)
+
+    if lista is None:
+        lista = novo
+        return lista
+    novo.proximo = lista
+    lista = novo
+    return lista
+
+
+def mostrar(lista):
+
+    if lista is None:
+        print("Lista vazia")
+        return
+    aux = lista
+    contador = 1
+
+    while aux is not None:
+
+        print(contador, "-", aux.nome)
+        contador += 1
+        aux = aux.proximo
+
+def menu():
+
+    print("\n===== ESCALAÇÃO =====")
+    print("1 - Adicionar jogador")
+    print("2 - Mostrar jogadores")
+    print("3 - Sair")
+
+    opc = int(input("Digite a opção: "))
+    return opc
+
+
+def main():
+
+    opc = 0
+    lista = None
+
+    while opc != 3:
+        opc = menu()
+
+        if opc == 1:
+            nome = input("Digite o nome do jogador: ")
+            lista = adicionar_nome(lista, nome)
+
+        elif opc == 2:
+            mostrar(lista)
+
+        elif opc == 3:
+            print("Saindo...")
+
+        else:
+            print("Opção inválida")
+
+
+main()
